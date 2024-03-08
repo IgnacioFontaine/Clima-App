2149437b40f95ce41b6b95efe06dd566c71c3f42
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-08 17:35:37 -0300

    configurando app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,27 @@
 import './App.css'
 
+//APIS para esta app:
+// APIS clima app:
+// -Openwheater
+
+// -Rest Countries
+// Endpoint todos los paises: https://restcountries.com/v3.1/all
+
+// -Spott
+
 function App() {
   
   return (
     <>
-      <h1>Clima App</h1>
+      <div>
+        <h1>Clima App</h1>
+      </div>
+      <div>
+        <h2>Select the countrie:</h2>
+        <select>
+          
+        </select>
+      </div>
     </>
   )
 }
