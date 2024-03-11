a4a94677919ff364cb810f205f2f398773e43fca
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-11 19:42:51 -0300

    clave en ENV

--- client/src/herramientas/getWeather.js
+++ client/src/herramientas/getWeather.js
@@ -3,7 +3,7 @@ import { ajax } from "./ajax";
 export const getWeather = async () => {
   const options = {
     method: 'GET',
-    url: 'api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=27b314f308c2f99c7e98a70b8529d801'
+    url: 'api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=ColocarClave'
   }
   
   return await ajax(options)
