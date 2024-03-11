9d2127e257c9101d6193c0119b2f8d020d96f1d2
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-11 16:46:05 -0300

    cambio get cityes

--- client/src/herramientas/getCities.js
+++ client/src/herramientas/getCities.js
@@ -2,8 +2,14 @@ import { ajax } from "./ajax";
 
 export const getCities = async (id) => {
   const options = {
-    method: 'GET',
-    url: `https://spott.p.rapidapi.com/places/${id}`,
+  method: 'GET',
+  url: `https://spott.p.rapidapi.com/places`,
+  params: {
+    type: 'CITY',
+    skip: '0',
+    country: `${id}`,
+    limit: '100',
+  },
   headers: {
     'X-RapidAPI-Key': '46ac1a98cdmsh1c008577cb9bc71p1a4984jsn7efc71b141f2',
     'X-RapidAPI-Host': 'spott.p.rapidapi.com'
