b281fcc950952f4e49ef0107ed5d70b7a8c6353a
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-12 10:39:20 -0300

    cambio get Wheater

--- client/src/herramientas/getWeather.js
+++ client/src/herramientas/getWeather.js
@@ -1,9 +1,14 @@
 import { ajax } from "./ajax";
 
+
 export const getWeather = async () => {
+  const API_KEY = "";
+  const lon = "";
+  const lat = "";
+  
   const options = {
     method: 'GET',
-    url: 'api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=ColocarClave'
+    url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
   }
   
   return await ajax(options)
