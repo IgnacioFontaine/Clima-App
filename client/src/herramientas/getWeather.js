aa183c843ffdb466c402de7bd41cceb42d7d23d6
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-12 12:59:02 -0300

    titulo cambiado

--- client/src/herramientas/getWeather.js
+++ client/src/herramientas/getWeather.js
@@ -6,7 +6,7 @@ export const getWeather = async ({lat,lon}) => {
 
   const options = {
     method: 'GET',
-    url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=27b314f308c2f99c7e98a70b8529d801`
+    url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
   }
   
   return await ajax(options)
