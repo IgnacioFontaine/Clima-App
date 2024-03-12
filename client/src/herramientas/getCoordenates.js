c22370aca3ae8122c68726490e2f5dec1633ab28
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-12 10:42:35 -0300

    get coordenates listo

---
+++ client/src/herramientas/getCoordenates.js
@@ -0,0 +1,14 @@
+import { ajax } from "./ajax";
+
+export const getCoordenates = async () => {
+  const options = {
+  method: 'GET',
+  url: 'https://countries-states-and-cities.p.rapidapi.com/cities',
+  params: {country: 'MX'},
+  headers: {
+    'X-RapidAPI-Key': '46ac1a98cdmsh1c008577cb9bc71p1a4984jsn7efc71b141f2',
+    'X-RapidAPI-Host': 'countries-states-and-cities.p.rapidapi.com'
+  }
+};
+  return await ajax(options)
+}
