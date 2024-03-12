6d2db06e8f424416325f381fb40cadfdc65ed6de
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-12 10:50:23 -0300

    cambio en get coordenates

--- client/src/herramientas/getCoordenates.js
+++ client/src/herramientas/getCoordenates.js
@@ -1,9 +1,12 @@
 import { ajax } from "./ajax";
 
 export const getCoordenates = async () => {
+  const API_KEY = "";
+  const country_code = "";
+  const city_name = "";
   const options = {
-  method: 'GET',
-  url: 'https://countries-states-and-cities.p.rapidapi.com/cities',
+  method: 'GET', 
+  url: `http://api.openweathermap.org/geo/1.0/direct?q=${city_name},{state code},${country_code}&limit=1&appid=${API_KEY}`,
   params: {country: 'MX'},
   headers: {
     'X-RapidAPI-Key': '46ac1a98cdmsh1c008577cb9bc71p1a4984jsn7efc71b141f2',
