d68148c8c8b04a7edcbeeb66c468feb2d1554392
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-12 11:09:53 -0300

    get argentinian coordenates

--- client/src/herramientas/getCoordenates.js
+++ client/src/herramientas/getCoordenates.js
@@ -4,14 +4,19 @@ export const getCoordenates = async () => {
   const API_KEY = "";
   const country_code = "";
   const city_name = "";
+
+  const options = {
+  method: 'GET',
+  url: `http://api.openweathermap.org/geo/1.0/direct?q=${city_name},${country_code}&limit=1&appid=${API_KEY}`
+};
+  return await ajax(options)
+}
+
+export const getArgentinianCoordenates = async () => {
+
   const options = {
   method: 'GET', 
-  url: `http://api.openweathermap.org/geo/1.0/direct?q=${city_name},{state code},${country_code}&limit=1&appid=${API_KEY}`,
-  params: {country: 'MX'},
-  headers: {
-    'X-RapidAPI-Key': '46ac1a98cdmsh1c008577cb9bc71p1a4984jsn7efc71b141f2',
-    'X-RapidAPI-Host': 'countries-states-and-cities.p.rapidapi.com'
-  }
+  url: `https://apis.datos.gob.ar/georef/api/provincias?nombre=cordoba`
 };
   return await ajax(options)
 }
