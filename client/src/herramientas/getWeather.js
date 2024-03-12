c1087295501a92492a8a774b96773db056c72c9f
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-12 13:23:34 -0300

    nueva variable de entorno

--- client/src/herramientas/getWeather.js
+++ client/src/herramientas/getWeather.js
@@ -1,12 +1,12 @@
 import { ajax } from "./ajax";
 
 
-export const getWeather = async ({lat,lon}) => {
-  const API_KEY = "";
+export const getWeather = async ({ lat, lon }) => {
+  
 
   const options = {
     method: 'GET',
-    url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+    url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${import.meta.env.VITE_APP_ID}`
   }
   
   return await ajax(options)
