e32c3860c3376e927d7b814e154ab521a32f8c8a
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-14 15:50:09 -0300

    cambios card wheater

--- client/src/Components/cardWheater.jsx
+++ client/src/Components/cardWheater.jsx
@@ -1,16 +1,18 @@
 
 
-function CardWheater(temp) {
+function CardWheater({ temp }) {
+  const { main, weather } = temp;
+
   
   return (
     <>
       <div>
-         <h3>Main: {temp.weather[0].main}</h3> 
+         <h3>Main: {weather? weather[0].main : null}</h3> 
          {/* <img>logoTemp</img>  */}
-         <h4>Temp: {temp.main.temp}° </h4>
-         <h5>Max temp: {temp.main.temp_max}° </h5>
-         <h5>Min temp: {temp.main.temp_min}° </h5>
-         <h5>humidity: {temp.main.humidity}% </h5>
+         <h4>Temp: {main? main.temp : null}° </h4>
+         <h5>Max temp: {main? main.temp_max : null}° </h5>
+         <h5>Min temp: {main? main.temp_min : null}° </h5>
+         <h5>humidity: {main? main.humidity : null}% </h5>
       </div>
     </>
   )
