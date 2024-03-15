a317c15da073bb4765332e439cf1ff10d6e04a38
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-14 22:27:07 -0300

    pequeño cambio en wheater card

--- client/src/Components/cardWheater.jsx
+++ client/src/Components/cardWheater.jsx
@@ -1,36 +1,31 @@
 
 
-function CardWheater({ temp, city }) {
-  const { main, weather } = temp;
-  const ciudad = city;
+function CardWheater({ temp }) {
+  const { main, weather, name } = temp;
 
   var iconcode = weather[0].icon;
   var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
 
+  var mapa_url = `./assets/Mapas/${name}.png`
+  var mapa_cba = "../assetsMapas/Córdoba.jpg"
+
   return (
     <>
-      <div style={{border:"1px solid gray",backdropFilter: 'blur(10px)'}} >
-        <div className="card-body p-4">
-            <div className="d-flex flex-row align-items-center">
+      <div style={{border:"1px solid gray", backgroundColor:"gray",backdropFilter: 'blur(10px)'}} >
+        <div >
+            <div >
               <div id="icon"><img id="wicon" src={iconurl} alt="Weather icon" /></div>
             </div>
           
-          <p className="mb-2">Temperatura actual: <strong>{main? main.temp : null}°C</strong></p>
-          <p>Max: <strong>{main? main.temp_max : null}°C</strong>, Min: <strong>{main? main.temp_min : null}°C</strong></p>
-          <p>Humedad: <strong>{main? main.humidity : null}%</strong></p>
+              <p  >Temperatura actual: <strong>{main? main.temp : null}°C</strong></p>
+              <p>Max: <strong>{main? main.temp_max : null}°C</strong>, Min: <strong>{main? main.temp_min : null}°C</strong></p>
+              <p>Humedad: <strong>{main? main.humidity : null}%</strong></p>
 
           </div>
-        </div>
-      
-
-      {/* <div>
-         <h3>Main: {weather? weather[0].main : null}</h3> 
-          <img>logoTemp</img>  
-         <h4>Temp: {main? main.temp : null}° </h4>
-         <h5>Max temp: {main? main.temp_max : null}° </h5>
-         <h5>Min temp: {main? main.temp_min : null}° </h5>
-         <h5>humidity: {main? main.humidity : null}% </h5>
-      </div>  */}
+      </div>
+      <div>
+        <div id="image"><img id="png" src={mapa_cba} alt="Mapa Ciudad" /></div>
+      </div>
     </>
   )
 }
