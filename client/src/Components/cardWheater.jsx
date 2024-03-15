0281a11512e8a56542df0b916b117a36a4f6f1af
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-15 14:36:36 -0300

    cambios visuales

--- client/src/Components/cardWheater.jsx
+++ client/src/Components/cardWheater.jsx
@@ -12,19 +12,18 @@ function CardWheater({ temp }) {
 
   return (
     <>
-      <div style={{ border: "1px solid gray", backdropFilter: 'blur(10px)' }} >
+      <div style={{ border: "1px solid gray", backgroundColor:"#C7FFFE",backdropFilter: 'blur(10px)', display:"flex", borderRadius:"4px" }} >
         <div>
           {ciudad[0]?(<div>
-          <div id="image"><img id="1" src={ciudad[0].mapa} style={{ height: "400px", width:"350px", p:1  }}  alt="Mapa Ciudad" /></div>
+          <div id="image"><img id="1" src={ciudad[0].mapa} style={{ height: "300px", width:"250px", p:1  }}  alt="Mapa Ciudad" /></div>
           </div>):(<div>
-            <img src={mapa_arg} style={{ height: "500px", width:"350px", p:1  }} ></img>
+            <img src={mapa_arg} style={{ height: "400px", width:"350px", p:1  }} ></img>
           </div>) }
         </div>
-        <div >
+        <div style={{height:"300px", width:"300px", p:1, justifyContent:"space-around"}} >
             <div >
               <div id="icon"><img id="wicon" src={iconurl} alt="Weather icon" /></div>
             </div>
-          
               <p  style={{color:"black"}} >Temperatura actual: <strong>{main? main.temp : null}°C</strong></p>
               <p style={{color:"black"}}>Max: <strong>{main? main.temp_max : null}°C</strong>, Min: <strong>{main? main.temp_min : null}°C</strong></p>
               <p style={{color:"black"}}>Humedad: <strong>{main? main.humidity : null}%</strong></p>
