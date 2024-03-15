3bf90487632dc57cdb60cac3324fecd4f0acf413
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-15 12:41:08 -0300

    todos los mapas importados y en uso

--- client/src/herramientas/cities.js
+++ client/src/herramientas/cities.js
@@ -13,31 +13,39 @@ import Neuquen_mapa from "../assets/Mapas/Neuquen.jpg"
 import SanLuis_mapa from "../assets/Mapas/SanLuis.jpg"
 import SantaCruz_mapa from "../assets/Mapas/SantaCruz.jpg"
 import Tucuman_mapa from "../assets/Mapas/Tucuman.jpg"
+import TierraDelFuego_mapa from "../assets/Mapas/TierraDelFuego.jpg"
+import Chubut_mapa from "../assets/Mapas/Chubut.jpg"
+import Formosa_mapa from "../assets/Mapas/Formosa.jpg"
+import RioNegro_mapa from "../assets/Mapas/RioNegro.jpg"
+import SanJuan_mapa from "../assets/Mapas/SanJuan.jpg"
+import SantaFe_mapa from "../assets/Mapas/SantaFe.jpg"
+import SantiagoDelEstero_mapa from "../assets/Mapas/SantiagoDelEstero.jpg"
+
 
 const argentinianCities =
   [ { id:1, name: "Buenos Aires", value:"La Plata",mapa: BuenosAires_mapa },
-    { id:2, name: "Ciudad Autonoma de Buenos Aires",value:"Ciudad Autonoma de Buenos Aires", mapa: BuenosAires_mapa },
+    { id:2, name: "Ciudad Autonoma de Buenos Aires",value:"Ciudad Autonoma de Buenos Aires", mapa:  },
     { id:3, name: "Catamarca", value:"San Fernando del Valle de Catamarca", mapa: BuenosAires_mapa},
     { id:4, name: "Chaco",value:"Resistencia", mapa: Chaco_mapa},
-    { id:5, name: "Chubut",value:"Rawson", mapa: BuenosAires_mapa},
+    { id:5, name: "Chubut",value:"Rawson", mapa: Chubut_mapa},
     { id:6, name: "Cordoba",value:"Cordoba", mapa: Córdoba_mapa},
     { id:7, name: "Corrientes",value:"Corrientes", mapa: Corrientes_mapa},
     { id:8, name: "Entre Rios",value:"Entre Rios", mapa: EntreRios_mapa},
-    { id:9, name: "Formosa",value:"Formosa", mapa: BuenosAires_mapa},
+    { id:9, name: "Formosa",value:"Formosa", mapa: Formosa_mapa},
     { id:10, name: "Jujuy",value:"San Salvador de Jujuy", mapa: Jujuy_mapa},
     { id:11, name: "La Pampa",value:"La Pampa", mapa: LaPampa_mapa},
     { id:12, name: "La Rioja",value:"La Rioja", mapa: LaRioja_mapa},
     { id:13, name: "Mendoza",value:"Mendoza", mapa: Mendoza_mapa},
     { id:14, name: "Misiones",value:"Misiones", mapa: Misiones_mapa},
     { id:15, name: "Neuquen",value:"Neuquen", mapa: Neuquen_mapa},
-    { id:16, name: "Rio Negro",value:"Rio Negro", mapa: BuenosAires_mapa},
+    { id:16, name: "Rio Negro",value:"Rio Negro", mapa: RioNegro_mapa},
     { id:17, name: "Salta",value:"Salta", mapa: Salta_mapa},
-    { id:18, name: "San Juan",value:"San Juan", mapa: BuenosAires_mapa},
+    { id:18, name: "San Juan",value:"San Juan", mapa: SanJuan_mapa},
     { id:19, name: "San Luis",value:"San Luis", mapa: SanLuis_mapa},
     { id:20, name: "Santa Cruz",value:"Santa Cruz", mapa: SantaCruz_mapa},
-    { id:21, name: "Santa Fe",value:"Santa Fe", mapa:BuenosAires_mapa },
-    { id:22, name: "Santiago del Estero",value:"Santiago del Estero", mapa:BuenosAires_mapa },
-    { id:23, name: "Tierra del Fuego", value:"Ushuaia", mapa: BuenosAires_mapa},
+    { id:21, name: "Santa Fe",value:"Santa Fe", mapa: SantaFe_mapa},
+    { id:22, name: "Santiago del Estero",value:"Santiago del Estero", mapa: SantiagoDelEstero_mapa},
+    { id:23, name: "Tierra del Fuego", value:"Ushuaia", mapa: TierraDelFuego_mapa},
     { id:24, name: "Tucuman",value:"San Miguel de Tucumán", mapa:Tucuman_mapa}]
 
 export default argentinianCities;
